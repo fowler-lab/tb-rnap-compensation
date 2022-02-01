90e22781cd48b5b648febec64b9d7e74f356feed
Philip W Fowler <philipwfowler@users.noreply.github.com>
2022-02-01 12:11:10 +0000

    bug fixes in asserts

--- tb_rnap_compensation/__init__.py
+++ tb_rnap_compensation/__init__.py
@@ -2,11 +2,13 @@
 
 import numpy
 
+from fisher import pvalue
+
 def calculate_fisher_pvalue(array):
 
-    assert isinstance(array, numpy.array), "have to pass a numpy array!"
+    assert isinstance(array, numpy.ndarray), "have to pass a numpy array!"
 
-    assert array.shape = (2,2), "has to be a 2x2 array!"
+    assert array.shape == (2,2), "has to be a 2x2 array!"
 
     # and use fisher.pvalue
     p = pvalue(array[0,0],array[0,1],array[1,0],array[1,1])
